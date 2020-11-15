89243f29863b73aaeb7003afd256f8b43b64666f
youyumeimei <64399063+youyumeimei@users.noreply.github.com>
2020-11-15 17:47:23 +0800

    Update main_matrix.cpp

--- main_matrix.cpp
+++ main_matrix.cpp
@@ -2,7 +2,6 @@
 // Created by 黄颖盈 on 2020/11/12.
 //
 
-#pragma (O3)
 #include <chrono>
 #include "matrix_multiply.h"
 
@@ -24,36 +23,47 @@ int main(){
         cout << "Please input the number of rows and columns of another matrix : ";
         cin >> matrixB.row >> matrixB.column;
 
+        matrixC.row=matrixA.row;
+        matrixC.column=matrixB.column;
+
         if (matrixA.column == matrixB.row) {
 
             matrixA.data = new float[matrixA.row * matrixA.column]();
             matrixB.data = new float[matrixB.row * matrixB.column]();
             matrixC.data = new float[matrixA.row * matrixB.column]();
 
-            float * vector_1;
-//            =new float [matrixA.column]();
-            float * vector_2;
-//            =new float [matrixA.column]();
-
+            cout<<"Please input items of the first matrix: ";
             for (int i = 0; i < matrixA.row * matrixA.column; ++i) {
-
-                cin >> *(matrixA.data+i);
+                string string1;
+                cin >> string1;
+                if (judge(string1)){
+                    *(matrixA.data+i)=stof(string1);
+                } else{
+                    cout<<"your input is wrong!"<<endl;
+                    return 0;
+                }
             }
+            cout<<"Please input items of the second matrix: ";
             for (int i = 0; i < matrixB.row * matrixB.column; ++i) {
-                cin >> *(matrixB.data+i);
+                string string1;
+                cin >> string1;
+                if (judge(string1)){
+                    *(matrixB.data+i)=stof(string1);
+                } else{
+                    cout<<"your input is wrong!"<<endl;
+                    return 0;
+                }
             }
 
             auto start = std::chrono::steady_clock::now();
-            matrix1(matrixA, matrixB,vector_1,vector_2);
+            matrix_manual(matrixA, matrixB,matrixC,matrixA.column);
             auto end = std::chrono::steady_clock::now();
             cout
-                    << "Slow calculations took "
+                    << "Matrix manual calculations took "
                     << chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "µs ≈ "
                     << chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms ≈ "
                     << chrono::duration_cast<std::chrono::seconds>(end - start).count() << "s.\n";
 
-            delete [] vector_1;
-            delete [] vector_2;
         } else {
             cout << "Your input is wrong!\nn v";
         }
@@ -62,36 +72,63 @@ int main(){
         matrixB.row=1000;
         matrixA.column=1000;
         matrixB.column=1000;
+        matrixC.row=1000;
+        matrixC.column=1000;
 
         matrixA.data=new float [1000*1000];
         matrixB.data=new float [1000*1000];
-        matrix2(matrixA,matrixB);
-        matrix2(matrixA,matrixB);
+        matrixC.data=new float [1000*1000];
+
+
         auto start = std::chrono::steady_clock::now();
-        matrix2(matrixA,matrixB);
+        matrix1(matrixA,matrixB,matrixC,matrixA.column);
         auto end = std::chrono::steady_clock::now();
         cout
-                << "Slow calculations took "
+                << "Matrix 1 calculations took "
                 << chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "µs ≈ "
                 << chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms ≈ "
                 << chrono::duration_cast<std::chrono::seconds>(end - start).count() << "s.\n";
 
-        matrix3(matrixA,matrixB);
-        matrix3(matrixA,matrixB);
         start = std::chrono::steady_clock::now();
-        matrix3(matrixA,matrixB);
+        matrix2(matrixA,matrixB,matrixC,matrixA.column);
         end = std::chrono::steady_clock::now();
         cout
-                << "Slow calculations took "
+                << "Matrix 2 calculations took "
                 << chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "µs ≈ "
                 << chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms ≈ "
                 << chrono::duration_cast<std::chrono::seconds>(end - start).count() << "s.\n";
 
+        start = std::chrono::steady_clock::now();
+        matrix3(matrixA,matrixB,matrixC,matrixA.column);
+        end = std::chrono::steady_clock::now();
+        cout
+                << "Matrix 3 calculations took "
+                << chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "µs ≈ "
+                << chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms ≈ "
+                << chrono::duration_cast<std::chrono::seconds>(end - start).count() << "s.\n";
 
-//        delete [] vector_1;
-//        delete [] vector_2;
+        start = std::chrono::steady_clock::now();
+        matrix4(matrixA,matrixB,matrixC,matrixA.column);
+        end = std::chrono::steady_clock::now();
+        cout
+                << "Matrix 4 calculations took "
+                << chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "µs ≈ "
+                << chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms ≈ "
+                << chrono::duration_cast<std::chrono::seconds>(end - start).count() << "s.\n";
+
+        start = std::chrono::steady_clock::now();
+        cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasTrans, 10000, 10000, 10000, 1.0, matrixA.data, 10000, matrixB.data, 10000, 0.0, matrixC.data, 10000);
+        end = std::chrono::steady_clock::now();
+
+        cout
+                << "Openblas took "
+                << chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "µs ≈ "
+                << chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms ≈ "
+                << chrono::duration_cast<std::chrono::seconds>(end - start).count() << "s.\n";
+
     }
 
     delete [] matrixA.data;
     delete [] matrixB.data;
+
 }
