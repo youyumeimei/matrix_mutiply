94a6001fbc0d6c9b5b5e6ea42bcebece46fde63c
youyumeimei <64399063+youyumeimei@users.noreply.github.com>
2020-11-15 17:51:03 +0800

    Update matrix_multiply.h

--- matrix_multiply.h
+++ matrix_multiply.h
@@ -4,9 +4,13 @@
 
 #ifndef ASSIGNMENT_MATRIX_MULTIPLY_H
 #define ASSIGNMENT_MATRIX_MULTIPLY_H
-#pragma (O3)
+//#pragma (O3)
+#pragma once
+#include <cstddef>
 #include <iostream>
-#include <String>
+#include <string>
+#include <cstring>
+#include <cblas.h>
 using namespace std;
 
 struct matrix{
@@ -16,9 +20,19 @@ struct matrix{
 };
 
 float dot_product1(const float *p1, const float * p2, size_t n);
-void matrix1(matrix matrixA,matrix matrixB,float * vector_1 ,float * vector_2);
-void matrix2(matrix matrixA, matrix matrixB);
-void matrix3(matrix matrixA, matrix matrixB);
+float dot_product2(const float *p1, const float * p2, size_t n);
+float dot_product3(const float *p1, const float * p2, size_t n);
+float dot_product4(const float *p1, const float * p2, size_t n);
+
+void matrix_manual(matrix matrixA,matrix matrixB,matrix matrixC,int n);
+void matrix1(matrix matrixA, matrix matrixB,matrix matrixC,int n);
+void matrix2(matrix matrixA, matrix matrixB,matrix matrixC,int n);
+void matrix3(matrix matrixA, matrix matrixB,matrix matrixC,int n);
+void matrix4(matrix matrixA, matrix matrixB,matrix matrixC,int n);
+
+bool judge(string string);
+void trans(matrix matrix);
+void output(matrix matrix);
 
 
 #endif //ASSIGNMENT_MATRIX_MULTIPLY_H
